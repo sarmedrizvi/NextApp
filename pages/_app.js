17585a0a9a15d5c669046fc3ac9263afcbb15d3a
sarmedrizvi <Max@Max-43.bizzwc.local>
2020-06-23 02:19:10 +0500

    Route change transition applied

--- pages/_app.js
+++ pages/_app.js
@@ -1,54 +1,42 @@
-import { PageTransition } from "next-page-transitions";
-
-import Loader from "../components/Loader";
-
-const TIMEOUT = 400;
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <PageTransition
-        timeout={TIMEOUT}
-        classNames="page-transition"
-        loadingComponent={<Loader />}
-        loadingDelay={500}
-        loadingTimeout={{
-          enter: TIMEOUT,
-          exit: 0,
-        }}
-        loadingClassNames="loading-indicator"
-      >
-        <Component {...pageProps} />
-      </PageTransition>
-      <style jsx global>{`
-        .page-transition-enter {
-          opacity: 0;
-          transform: translate3d(0, 20px, 0);
-        }
-        .page-transition-enter-active {
-          opacity: 1;
-          transform: translate3d(0, 0, 0);
-          transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
-        }
-        .page-transition-exit {
-          opacity: 1;
-        }
-        .page-transition-exit-active {
-          opacity: 0;
-          transition: opacity ${TIMEOUT}ms;
-        }
-        .loading-indicator-appear,
-        .loading-indicator-enter {
-          opacity: 0;
-        }
-        .loading-indicator-appear-active,
-        .loading-indicator-enter-active {
-          opacity: 1;
-          transition: opacity ${TIMEOUT}ms;
-        }
-      `}</style>
-    </>
-  );
+import App, { Container } from 'next/app'
+import React from 'react'
+import { PageTransition } from 'next-page-transitions'
+ 
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+ 
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+ 
+    return { pageProps }
+  }
+ 
+  render() {
+    const { Component, pageProps,router } = this.props
+    return (
+      <Container>
+        <PageTransition timeout={300} classNames="page-transition">
+          <Component {...pageProps} key={router.route} />
+        </PageTransition>
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transition: opacity 300ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+        `}</style>
+      </Container>
+    )
+  }
 }
-
-export default MyApp;
